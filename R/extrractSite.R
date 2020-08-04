f0196908ed9d9b0f93f0652d638eb083797da1e5
Chengyang Ji <chengyang.ji12@alumni.xjtlu.edu.cn>
2020-08-04 08:55:18 +0000

    Documentation on 'extractSite' function

--- R/extrractSite.R
+++ R/extrractSite.R
@@ -1,10 +1,20 @@
-#' @rdname extractTips
+#' @rdname extractSite
 #' @name extractSite
-#' @description The function \code{extractSite} can be used to extract the
-#'   fixation info of a single site.
-#' @return The function \code{extractSite} returns a \code{sitePath} object
+#' @title Extract tips for a single site
+#' @description The functions in \code{sitePath} usually include the results on
+#'   more than one site. The function \code{extractSite} can be used to extract
+#'   the predicted result on a single site.
+#' @param x A \code{fixationSites} or a \code{parallelSites} object. More type
+#'   will be supported in the later version.
+#' @param site A site included in the result.
+#' @param ... Other arguments
+#' @return The predicted result of a single site
 #' @export
 #' @examples
+#' data(zikv_tree_reduced)
+#' data(zikv_align_reduced)
+#' tree <- addMSA(zikv_tree_reduced, alignment = zikv_align_reduced)
+#' mutations <- fixationSites(lineagePath(tree))
 #' extractSite(mutations, 139)
 extractSite.fixationSites <- function(x, site, ...) {
     return(.actualExtractSite(x, site))
@@ -21,7 +31,7 @@ extractSite.fixationSites <- function(x, site, ...) {
     return(sp)
 }
 
-#' @rdname extractTips
+#' @rdname extractSite
 #' @name extractSite
 #' @export
 extractSite.multiFixationSites <- function(x, site, ...) {
